37a7002579e71e11ef5cf6175efd5fe8bcaa55a7
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-10-06 08:39:36 -0700

    additional syntax

--- loan-app/src/app/home/home.component.ts
+++ loan-app/src/app/home/home.component.ts
@@ -41,29 +41,36 @@ export class HomeComponent implements OnInit {
     return this.loanForm.controls;
   };
 
-calculateResults() {
-  // Create a variable that hold the loan amount entered by user.
-  let loanAmount = parseFloat(this.loanForm.value.loanAmount);
+  calculateResults() {
+    // Create a variable that hold the loan amount entered by user.
+    let loanAmount = parseFloat(this.loanForm.value.loanAmount);
 
-  // Create a variable that holds the loan term in years entered by the user.
-  let numOfYears = parseFloat(this.loanForm.value.numOfYears);
+    // Create a variable that holds the loan term in years entered by the user.
+    let numOfYears = parseFloat(this.loanForm.value.numOfYears);
 
-  // Create a variable that holds the interest rate entered by the user.
-  let interestRate = parseFloat(this.loanForm.value.interestRate);
+    // Create a variable that holds the interest rate entered by the user.
+    let interestRate = parseFloat(this.loanForm.value.interestRate);
 
-  // create a variable with the value of the interest rate per month as a decimal.
-  let i = ((interestRate / 100) / 12);
+    // create a variable with the value of the interest rate per month as a decimal.
+    let i = ((interestRate / 100) / 12);
 
-  // Create a variable the loan term in months.
-  let numOfMonths = (numOfYears * 12);
+    // Create a variable the loan term in months.
+    let numOfMonths = (numOfYears * 12);
 
-  // Calculate the monthly payment based on values entered by the user.
-  this.monthlyPayment = (((loanAmount * i * Math.pow((1 + i), numOfMonths)))/((Math.pow((1 + i), numOfMonths)) - 1));
+    // Calculate the monthly payment based on values entered by the user.
+    this.monthlyPayment = (((loanAmount * i * Math.pow((1 + i), numOfMonths)))/((Math.pow((1 + i), numOfMonths)) - 1));
 
-  // Calculate the total interest paid that will have to be paid based on values entered by the user user.
-  this.interestPaid = ((this.monthlyPayment * numOfMonths) - loanAmount);
-}
+    // Calculate the total interest paid that will have to be paid based on values entered by the user user.
+    this.interestPaid = ((this.monthlyPayment * numOfMonths) - loanAmount);
+  }
 
+  // Clear form and reset interest paid and monthly payment values back to 0
+  clearForm() {
+    this.loanForm.reset();
+    this.interestPaid = 0;
+    this.monthlyPayment = 0;
+
+  }
 }
 
 
